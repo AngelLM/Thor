69ad11a885bd2f695971236c26fe5bebc149d9d8
AngelLM <alm37454@gmail.com>
2015-12-29 18:11:05 +0100

    FreeCAD Files & firmware update

--- fw/Art123/Marlin/Configuration.h
+++ fw/Art123/Marlin/Configuration.h
@@ -587,7 +587,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {44.9,240,5071,500}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {44.9,240,50.71,500}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {250, 250, 250, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {3000,3000,3000,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
